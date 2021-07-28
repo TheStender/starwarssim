3dd4135acb28e20065996fc4286b651949e1d401
Anthony Stender <anthony.m.stender@gmail.com>
2021-07-28 11:29:32 -0500

    More testing

---
+++ src/GameScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function GameScreen() {
+  return (
+    <div className='GameScreen'>
+      <header>
+        <p>STAR WARS SIM!</p>
+        <p>Simming!</p>
+      </header>
+    </div>
+  );
+}
+
+export default GameScreen;
